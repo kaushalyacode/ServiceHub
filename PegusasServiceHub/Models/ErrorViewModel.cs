13d8529f41c668dc8ec0a1b7d29a28d40f9a17cd
kaushalyacode <harshanathwak@gmail.com>
2026-05-30 22:55:36 -0400

    Initial PegusasServiceHub project

---
+++ PegusasServiceHub/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PegusasServiceHub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
